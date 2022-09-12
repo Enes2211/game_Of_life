1363005ccd57b4e8809e0721e09aa17e745a8be7
Enes2211 <Student897@tumo.world>
2022-09-12 19:50:17 +0200

    learnt more on json and objects

---
+++ script.js
@@ -0,0 +1,11 @@
+var obj ={
+    "first_name": "Vardan",
+    "last_name" : "Hovsepyan",
+    "age": 13,
+    "tumo_Student": true
+}
+
+console.log(obj.first_name)
+console.log(obj.last_name)
+console.log(obj.age)
+console.log(obj.tumo_Student)
